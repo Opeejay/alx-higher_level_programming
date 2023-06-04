16823a887711ec3cd97d6a1cfe20f6b85c182f8b
Opeejay <peterodinka@yahoo.com>
2023-06-04 14:48:46 -0700

    7-rectangle.py

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -3,12 +3,14 @@
 
 
 class Rectangle:
-    """Defines a reactangle.
+    """Defines a rectangle.
     Attributes:
         number_of_instances (int): The number of Rectangle instances.
+        print_symbol (any): The symbol used for string representation.
     """
 
     number_of_instances = 0
+    print_symbol = '#'
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
@@ -22,12 +24,12 @@ class Rectangle:
 
     @property
     def width(self):
-        """Return width of the rectangle """
+        """Retrieves the value of `width`"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Set width of the rectangle."""
+        """Sets the width of the rectangle."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -36,12 +38,12 @@ class Rectangle:
 
     @property
     def height(self):
-        """Return height of the rectangle."""
+        """Retrieves the value of `height`."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Set height of the rectangle. """
+        """Sets the height of the rectangle."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -49,11 +51,11 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """Return the area of the Rectangle."""
+        """Calculates the area of a reactangle."""
         return (self.__width * self.__height)
 
     def perimeter(self):
-        """Return the perimeter of the Rectangle."""
+        """Calculates the perimeter of a reactangle."""
         if self.__width == 0 or self.__height == 0:
             return (0)
         return (2*(self.__width + self.__height))
@@ -67,7 +69,7 @@ class Rectangle:
 
         rect = []
         for i in range(self.__height):
-            [rect.append('#') for j in range(self.__width)]
+            [rect.append(str(self.print_symbol)) for j in range(self.__width)]
             if i != self.__height - 1:
                 rect.append('\n')
         return ("".join(rect))
