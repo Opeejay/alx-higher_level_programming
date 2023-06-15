d80c62fed898fa54eb66b7a350520b54e2eb43bf
Opeejay <peterodinka@yahoo.com>
2023-06-15 22:31:43 +0000

    10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,5 @@
 #!/usr/bin/node
-
 function factorial (n) {
-  if (n === 0 || isNaN(n)) {
-    return 1;
-  } else {
-    return n * factorial(n - 1);
-  }
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
 }
-
-const num = Number(process.argv[2]);
-console.log(factorial(num));
+console.log(factorial(Number(process.argv[2])));
